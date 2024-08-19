9acbadf3f6c613eb220e20e4bbeff40ff777a6f3
ston <francuz2103@gmail.com>
2024-08-19 19:51:55 +0500

    added login and password forms

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
+from django import forms
+
+
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': "form-control py-4", 'placeholder': 'Введите пароль'
+    }))
+
     class Meta:
         model = User
         fields = ('username', 'password')
