c6355202a9b698c00531da5c17ad69492c1744b3
ston <francuz2103@gmail.com>
2024-08-19 17:16:26 +0500

    added the user authorization logic to the controller

--- users/views.py
+++ users/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
+
+
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
 
 def registration(request):
     return render(request, 'users/registration.html')
